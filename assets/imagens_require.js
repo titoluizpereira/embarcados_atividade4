83257418e4bb3b7dae99538701f7dd084f2702fc
Tito L. Pereira <150470387+titoluizpereira@users.noreply.github.com>
2025-11-21 18:03:11 -0300

    deletei coisa inutil

--- assets/imagens_require.js
+++
@@ -1,9 +0,0 @@
-export default {
-  basquete: require('./imagens/basquete.jpg'),
-  futebol: require('./imagens/futebol.jpeg'),
-  volei: require('./imagens/volei.jpg'),
-  natacao: require('./imagens/natacao.jpg'),
-  atletismo: require('./imagens/atletismo.jpg'),
-};
-
-
